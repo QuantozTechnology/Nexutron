6a9e158060662b4b1eb5ad499f84d8126fc3510a
Paul van Grol <paul@pvangrol.nl>
2024-05-15 14:54:31 +0200

    Split out some tests

--- TestTronDotNet/Program.cs
+++ TestTronDotNet/Program.cs
@@ -1,13 +1,11 @@
 ﻿using Google.Protobuf;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
-using System;
-using System.Threading.Tasks;
 using Nexutron;
 using Nexutron.Contracts;
-using Nexutron.Crypto;
 using Nexutron.Extensions;
 using Nexutron.Protocol;
+using System;
 
 namespace TestTronDotNet
 {
@@ -17,7 +15,7 @@ namespace TestTronDotNet
         {
             Console.WriteLine("Hello NexuTron World!");
 
-            TestSigning();
+            TestTransactionParsing();
         }
 
 
@@ -35,6 +33,7 @@ namespace TestTronDotNet
 
         public static void TestKeyGeneration()
         {
+            // Should be TBDCyrZ1hT1PDDFf2yRABwPrFica5qqPUX
             var key = new TronECKey("fd605fb953fcdabb952be161265a75b8a3ce1c0def2c7db72265f9db9a471be4", TronNetwork.MainNet);
 
             var address = key.GetPublicAddress();
@@ -47,12 +46,12 @@ namespace TestTronDotNet
         public static void TestSigning()
         {
 
-            var walletPrivateKey = "0000000000000000000000000000000000000000000000000000000000000001";
-
+            var walletPrivateKey = "fd605fb953fcdabb952be161265a75b8a3ce1c0def2c7db72265f9db9a471be4";
 
 
             var ecKey = new TronECKey(walletPrivateKey, TronNetwork.MainNet);
             var from = ecKey.GetPublicAddress();
+
             //Receiving wallet
             var to = "TEiMQZpHs4N4HuTKP3xcCKZ68XSQSfEbMW";
 
@@ -63,7 +62,7 @@ namespace TestTronDotNet
             IServiceCollection services = new ServiceCollection();
             services.AddTronDotNet(x =>
             {
-                x.Network = TronNetwork.MainNet;
+                x.Network = TronNetwork.TestNet;
                 x.Channel = new GrpcChannelOption { Host = "3.225.171.164", Port = 50051 };
                 x.SolidityChannel = new GrpcChannelOption { Host = "3.225.171.164", Port = 50052 };
                 // x.ApiKey = "07rgc8e4-7as1-4d34-d334-fe40ai6gc542";
@@ -74,6 +73,7 @@ namespace TestTronDotNet
             services.AddLogging();
             var service = services.BuildServiceProvider();
             var transactionClient = service.GetService<ITransactionClient>();
+
             var transactionExtension = transactionClient.CreateTransactionAsync(from, to, amount).Result;
 
             var transactionSigned = transactionClient.GetTransactionSign(transactionExtension.Transaction, walletPrivateKey);
@@ -91,6 +91,41 @@ namespace TestTronDotNet
             var result = transactionClient.BroadcastTransactionAsync(transactionSigned).Result;
             Console.WriteLine("-RESULT-");
             Console.WriteLine(JsonConvert.SerializeObject(result));
+
+            Console.Write("-DONE-");
+
+        }
+
+        public static void TestTransactionParsing()
+        {
+
+
+            IServiceCollection services = new ServiceCollection();
+            services.AddTronDotNet(x =>
+            {
+                x.Network = TronNetwork.TestNet;
+                x.Channel = new GrpcChannelOption { Host = "grpc.shasta.trongrid.io", Port = 50051 };
+                x.SolidityChannel = new GrpcChannelOption { Host = "grpc.shasta.trongrid.io", Port = 50052 };
+                //x.Channel = new GrpcChannelOption { Host = "3.225.171.164", Port = 50051 };
+                //x.SolidityChannel = new GrpcChannelOption { Host = "3.225.171.164", Port = 50052 };
+                // x.ApiKey = "07rgc8e4-7as1-4d34-d334-fe40ai6gc542";
+                //I thought it was necessary to fill in, but it seems that it can be used without filling in
+                x.ApiKey = "apikey";
+            });
+
+            services.AddLogging();
+            var service = services.BuildServiceProvider();
+            var walletClient = service.GetService<IWalletClient>();
+            var wallet = walletClient.GetProtocol();
+
+            // https://shasta.tronscan.org/#/transaction/17821228a79904c23bd35e566f320c2d43e6940c0d44bc8d70f257f3485459bb
+            var trxTx = wallet.GetTransactionById(new BytesMessage { Value = ByteString.FromBase64("F4ISKKeZBMI7015WbzIMLUPmlAwNRLyNcPJX80hUWbs=") });
+            var contractTx = wallet.GetTransactionById(new BytesMessage { Value = ByteString.FromBase64("/MLD7DLhMWW6oVzDO7Jefzoeqzg5ehi0wPPeDkFPpSw=") });
+
+            var trxTxUnpacked = trxTx.RawData.Contract[0].Parameter.Unpack<TransferContract>();
+            var contractTxUnpacked = contractTx.RawData.Contract[0].Parameter.Unpack<TriggerSmartContract>();
+
+            Console.Write("-DONE-");
 
         }
 
